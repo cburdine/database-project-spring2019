df0f73c035a5acbd35d723e4b2c2229555c971c1
JakeBerns <JakeBerns28@gmail.com>
2019-04-24 21:32:32 -0500

    progressing on our edit functions

---
+++ src/model/edit_goal_info.py
@@ -0,0 +1,16 @@
+from src.model import client_model
+from src.model import classes
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="jabtexas23",
+    database="Curricula")
+
+my_cursor = mydb.cursor()
+
+def edit_goal(goal_id):
+    """Function to edit goal"""
+
+    return 'nyc'
