ef4a7a246cbf54b4b42f62a07f65f0634ed8e73c
JakeBerns <jakeberns28@gmail.com>
2019-04-24 21:06:43 -0500

    functionality to edit an existing curriculum

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 #!usr/bin/python3
 
 from src.model.app import CurriculaApp
+from src.model.enter_new_curriculum import enter_new_curriculum
 
 def main():
     # import/build .kv file:
     CurriculaApp().run()
-
 
 if __name__ == '__main__':
     main()
